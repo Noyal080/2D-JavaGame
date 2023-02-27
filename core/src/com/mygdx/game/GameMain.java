7392feb9e15dde61f3ccdc70c9531855f493604c
Noyal080 <n.nakarmi22@gmail.com>
2023-02-27 13:50:38 +0545

    update

--- core/src/com/mygdx/game/GameMain.java
+++ core/src/com/mygdx/game/GameMain.java
@@ -1,4 +1,35 @@
 package com.mygdx.game;
 
-public class GameMain {
+import com.badlogic.gdx.Game;
+import com.mygdx.game.model.World;
+
+public class GameMain extends Game {
+private World world;
+public static final String VERSION = "0.03";
+public static final int FPS =60;
+public void create()
+{
+
 }
+public void dispose()
+{
+    world.dispose();
+}
+public void resize(int width, int height)
+{
+    world.getRenderer().resize(width,height);
+}
+public void pause()
+{
+
+}
+public void resume()
+{
+
+}
+public void render()
+{
+    world.update();
+}
+}
+
--- core/src/com/mygdx/game/model/World.java
+++ core/src/com/mygdx/game/model/World.java
@@ -1,4 +1,55 @@
 package com.mygdx.game.model;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import com.mygdx.game.background.WorldRenderer;
+
 public class World {
+    private static final float Gravity = -150;
+    public static final float scale = 1 / 16f;
+
+    private WorldRenderer wr;
+    private Pool<Rectangle> rectPool = new Pool<Rectangle>() {
+        protected Rectangle newObject() {
+            return new Rectangle();
+
+        }
+    };
+    //    private Stage stage;
+    //reset
+    private static boolean reset_flag = false;
+    private boolean playing_song = false;
+
+    //    private Flag = flag;
+    private static Array<Actor> objectToRemove = new Array<Actor>();
+
+    public World() {
+        reset();
+    }
+    public WorldRenderer getRenderer()
+    {
+        return wr;
+    }
+
+    private boolean level_ended = false;
+
+    public void removeActor(Actor a) {
+
+    }
+
+    private void reset() {
+
+    }
+
+    public void update()
+    {
+
+    }
+    public void dispose()
+    {
+
+    }
+
 }
